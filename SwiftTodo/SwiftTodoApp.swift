ad92ff5387ba9fee4e6834bed353608ce20e2ce8
@manuelduarte077 <carlosd99m@gmail.com>
2022-05-01 20:54:37 -0600

    initial commit

---
+++ SwiftTodo/SwiftTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTodoApp.swift
+//  SwiftTodo
+//
+//  Created by Manuel Duarte on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
